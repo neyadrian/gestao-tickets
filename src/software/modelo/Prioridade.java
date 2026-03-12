6d359790a7cc91cf4dddea8cd0ed9b634ca1a41e
neyadrian <neyadrian2018@gmail.com>
2026-03-12 15:56:01 -0300

    prioridade do sistema

--- src/software/modelo/Prioridade.java
+++ src/software/modelo/Prioridade.java
@@ -1,7 +1,20 @@
 package software.modelo;
 
 public class Prioridade {
-    public enum TipoPrioridade {BAIXA, MEDIA, ALTA, URGENTE}
-    private Status status;
+    public enum TipoPrioridade {
+        BAIXA(72),
+        MEDIA(48),
+        ALTA(24),
+        URGENTE(4);
 
+        private final int prazoHoras;
+
+        Prioridade(int prazoHoras) {
+            this.prazoHoras = prazoHoras;
+        }
+
+        public int getPrazoHoras() {
+            return prazoHoras;
+        }
+    }
 }
